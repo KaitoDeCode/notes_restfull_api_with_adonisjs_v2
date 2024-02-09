37c3fec9c3487d28763c7eeeadf746554253952c
KaitoDeCode <kurniawanadi4556@gmail.com>
2024-02-09 17:40:57 +0700

    make method store for note

--- app/controllers/notes_controller.ts
+++ app/controllers/notes_controller.ts
@@ -4,12 +4,22 @@ import Note from "#models/note";
 import { HttpContext } from "@adonisjs/core/http";
 
 export default class NotesController {
-  async index(ctx: HttpContext){
 
+  async index(ctx: HttpContext){
     const data = await Note.all();
     return ctx.response.status(200).json({
       message:"Success",
       data
     })
   }
+
+  async store(ctx: HttpContext){
+    const data:Object = ctx.request.only(['title','contents'])
+    const result = await Note.create(data)
+    return ctx.response.status(200).json({
+      message:"Success",
+      data : result
+    })
+  }
+
 }
--- start/routes.ts
+++ start/routes.ts
@@ -12,5 +12,6 @@ import router from '@adonisjs/core/services/router'
 
 router.group(()=>{
   router.get('/notes',[NotesController,'index']).as('get.notes')
-  
+  router.post('/create-notes',[NotesController,'store']).as('store.notes')
+
 }).prefix('api/v1')
