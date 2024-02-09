32616bc669efd03c6454659f1ae3653df8fa04a6
KaitoDeCode <kurniawanadi4556@gmail.com>
2024-02-09 17:03:08 +0700

    update note controller

--- app/controllers/notes_controller.ts
+++ app/controllers/notes_controller.ts
@@ -1,9 +1,9 @@
 // import type { HttpContext } from '@adonisjs/core/http'
 
-import { HttpContext } from "@adonisjs/core/http";
 import Note from "#models/note";
+
 export default class NotesController {
-  async index(ctx: HttpContext){
+  async index(){
     return await Note.all()
   }
 }
